9d528f39f4c8a711bf44d518f3d038c29d1e4ebe
unknown <fahox1991@gmail.com>
2024-01-25 16:42:20 +0100

    initial commit

---
+++ src/main/java/com/ConnectFour/ConnectFourGame/ConnectFourGameApplication.java
@@ -0,0 +1,13 @@
+package com.ConnectFour.ConnectFourGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFourGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFourGameApplication.class, args);
+	}
+
+}
